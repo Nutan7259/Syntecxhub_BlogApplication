65d856f4cfc92c24c388f085d8a16c308603b1c6
Nutan <rekharastekar12@gmail.com>
2026-02-04 22:50:13 +0530

    cors in server

--- Backend/server.js
+++ Backend/server.js
@@ -24,15 +24,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://syntecxhub-blog-application-5o67.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
+app.options("*", cors());
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
